306e7b1efec0b3e3899dfdf1197ea879258cad93
vikaskamineni <vikaskamineni7@gmail.com>
2017-11-15 02:52:56 +0530

    Added all files

---
+++ shared_folder1/client1.py
@@ -0,0 +1,516 @@
+import socket
+import os
+import hashlib
+import time
+import platform
+from datetime import datetime
+import re
+import json
+from stat import *
+def hash_value_func(filename):
+	fil = open(filename, 'rb') # rb indicate - open a file for reading
+	hashval = hashlib.md5(fil.read()).hexdigest()
+	return hashval
+def hash_verify(s,filename):
+	#print filename
+	recv_hash = s.recv(1024)
+	return recv_hash
+def hash_checkall(s):
+	while True:
+		data = s.recv(1024)
+		#s.send("gotit")
+		filename = s.recv(1024)
+		#s.send("gotfile")
+		hash_value = hash_verify(s,filename)
+		if data == 'done':
+			break
+		print 'last modified time:' + data + '\t' + 'hash_value:' + hash_value + '\t' + filename
+def directorydownload(s,protocol,directory):
+	if os.path.exists(directory):
+		#cmd = 'cd ' + directory
+		#print 'made'
+		os.chdir(directory)
+	else:
+		#print 'make'
+		#cmd = 'mkdir ' + directory
+		os.mkdir(directory)
+		os.chdir(directory)
+	s.send('change directory ' + directory)
+	data = s.recv(1024)
+	#print data
+	if data != 'changed':
+		print data
+	s.send('send hashes')
+	file_array = []
+	hash_array = []
+	while True:
+		data = s.recv(1024)
+		#print data
+		data1 = data.split('\t')
+		if data == 'done':
+			break
+		s.send('recieved')
+		file_array.append(data1[1])
+		hash_array.append(data1[0])
+	#print file_array
+	#print hash_array
+	i=0
+	while i<len(file_array):
+		#print 'entered'
+		hash_value = hash_array[i]
+		#print hash_value
+		if hash_value == '-1':
+			directorydownload(s,protocol,file_array[i])
+		else:
+			args = 'download ' + protocol + ' ' + file_array[i]
+			result = filedownload(s,args,file_array[i],protocol)
+			#print result
+			fil = open(file_array[i], 'rb') # rb indicate - open a file for reading
+			sent_hash = hashlib.md5(fil.read()).hexdigest()
+			result1 = result.split()
+			#print result1
+			if result1[0] == 'nodatasent':
+				command = 'chmod ' + result1[1] + ' ' + file_array[i]
+				#print command
+				os.system(command)
+				print(file_array[i] +':file already existing in the folder and uptodate')
+			else:
+				if result != sent_hash:
+					print(file_array[i]+':download failed hash not same')
+				else:
+					#s.send('send_status')
+					data = s.recv(1024)
+					spl = data.split('\t')
+					perm = spl[4]
+					#print('permissions:' + perm)
+					command = 'chmod ' +perm + ' ' + file_array[i]
+					#print command
+					os.system(command)
+					permissions = str(oct(os.stat(file_array[i])[ST_MODE])[-3:])
+					#print permissions
+					#print data
+					print('hash:' + result + '\t' + data)
+					#print 'Downloading completed successfully'
+					print(file_array[i]+':file downloaded successfully')
+		i = i+1
+	os.chdir('..')
+	s.send('change directory ' + '..')
+	data = s.recv(1024)
+	#print data
+	if data != 'changed':
+		print data
+
+def filedownload(s,args , filename , protocol):
+	#print protocol
+	flag = 2
+	poi = 0
+	s.send(args)
+	data = s.recv(1024)
+	#print data
+	if data != 'recieved':
+		#print 'abc'
+		print data
+	if protocol!='UDP' and protocol!='TCP':
+		print 'wrong protocol argument'
+		return
+	#if protocol == 'UDP':
+		#has to create a new socket ns with SOCK_DGRAM
+	#	new_port = int(s.recv(1024))
+	#	ns = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	#	ns.close()
+	if protocol == 'UDP':
+		new_port = (s.recv(1024))
+		#print "ssss"+new_port
+		ns = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		addr = (host,int(new_port))
+		ns.sendto("recieved", addr)
+		recv_hash1 = ns.recvfrom(1024)
+		#print recv_hash1
+		#recv_hash2 = recv_hash1.split()
+		recv_hash = recv_hash1[0]
+		#print recv_hash 
+		cmd = "ls"
+		#os.system(cmd)
+		result = os.popen(cmd).read()
+		files = result.split()
+		#print files
+		num = len(files)
+		#print num
+		i=0
+		while i<num:
+			if(files[i] == filename):
+				poi = 1
+				hash_value_in = hash_value_func(files[i])
+				hash_value_orig = recv_hash
+				if hash_value_in == hash_value_orig:
+					flag = 1
+					#print 'hashsame'
+					ns.sendto('hashsame',addr)
+				elif hash_value_in != hash_value_orig:
+					flag = 0
+					#print 'hashnotsame'
+					ns.sendto('hashnotsame',addr)
+					#print 'hashnotsame'
+				break
+			i = i+1
+		if poi == 0:
+			flag = 2
+			ns.sendto('newfile',addr)
+		if flag == 0:
+			cmd1 = "rm " + filename
+			os.system(cmd1)
+			command = "touch " + filename
+			#print command
+			os.system(command)
+			try:
+				f = open(filename,'wb+')
+			except:
+				print "no space"
+				return
+			while True:
+				data, addr = ns.recvfrom(1024)
+				if data == 'done':
+					break
+				f.write(data)
+				ns.sendto("recieved",addr)
+			return recv_hash
+		elif flag == 1:
+			data1 = ns.recvfrom(1024)
+			#print data1
+			data = data1[0]
+			#print data
+			return data
+		elif flag == 2:
+			try:
+				f = open(filename,'wb+')
+			except:
+				print "no space"
+				return
+			while True:
+				data, addr = ns.recvfrom(1024)
+				if data == 'done':
+					break
+				f.write(data)
+				ns.sendto("recieved",addr)
+			return recv_hash
+		f.close()
+		ncs.close()
+
+	if protocol == 'TCP':
+		#can use the old socket s
+		recv_hash = s.recv(1024)
+		cmd = "ls"
+		#os.system(cmd)
+		result = os.popen(cmd).read()
+		files = result.split()
+		#print files
+		num = len(files)
+		#print num
+		i=0
+		while i<num:
+			if(files[i] == filename):
+				poi = 1
+				hash_value_in = hash_value_func(files[i])
+				hash_value_orig = recv_hash
+				if hash_value_in == hash_value_orig:
+					flag = 1
+					s.send('hashsame')
+				elif hash_value_in != hash_value_orig:
+					flag = 0
+					s.send('hashnotsame')
+				break
+			i = i+1
+		if poi == 0:
+			flag = 2
+			s.send('newfile')
+		if flag == 0:
+			cmd1 = "rm " + filename
+			os.system(cmd1)
+			command = "touch " + filename
+			#print command
+			os.system(command)
+			try:
+				fil = open(filename,'wb+') # wb+ indicate - create a file which can be updated
+			except:
+				print "cannot open the file"
+				return
+			while True:
+				data = s.recv(1024)
+				if data == 'done':
+					break
+				fil.write(data)
+				s.send('recieved')
+				fil.close()
+				#s.close()
+			return recv_hash
+		elif flag == 1:
+			#while True:
+			#	data = s.recv(1024)
+			#	if data == 'done':
+			#		break
+			#	s.send('recieved')
+			data = s.recv(1024)
+			#print data
+			return data
+		elif flag == 2:
+			#print '2'
+			try:
+				fil = open(filename,'wb+') # wb+ indicate - create a file which can be updated
+			except:
+				print "cannot open the file"
+				return
+			while True:
+				#print 'transfer'
+				data = s.recv(1024)
+				if data == 'done':
+					break
+				fil.write(data)
+				s.send('recieved')
+				fil.close()
+				#s.close()
+			return recv_hash
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = input("enter port number: ") # Enter the port of server2 - folder2 server
+host = raw_input("Enter Host-ip: ") 
+sharedfolder1 = raw_input("Shared Folder1: ") # Full path the folder you wish to share 
+#port = 6001
+#host = '127.0.0.1'
+#sharedfolder1 = 'folder1'
+if not os.path.exists(sharedfolder1):
+	print "No Such Folder"
+	exit(0)
+elif not os.access(sharedfolder1, os.W_OK):
+	print "No Privilleges"
+	exit(0)
+else:
+	os.chdir(sharedfolder1)
+#try:
+#    log = open("client_log.log", "a+")
+#except:
+#    print "Cannot Open Log file"
+#    exit(0)
+try: 
+	s.connect((host,port))
+except:
+	print 'No available server with that Host-ip'
+	s.close()
+	exit(0)
+cnt = 0
+print 'Connection established'
+mode = 1
+strt_flag = input("Enter the mode sync(1) prompt(0) \nprompt> ")
+while True:
+	cnt += 1
+	#curr_time = time.time()
+	#curr_time1 = curr_time
+	#while curr_time1 - curr_time<=5:
+	#	strt_flag = 1
+	#	curr_time1 = time.time()
+	#mode = raw_input("prompt> ")
+	#s.send(mode)
+	#data = s.recv(1024)
+	#if data != 'modeset':
+	#	print data
+	if strt_flag == 1 :
+		#strt_flag = 0
+		args = 'hash checkall'
+		#s.send('send hashes')
+		#data = s.recv(1024)
+		#print data
+		#if data != 'recieved':
+		#	print data
+		#hash_array = []
+		#name_array = []
+		#while True:
+		#	data = s.recv(1024)
+		#	if data == 'done':
+		#		break
+		#	s.send('recieved')
+		#	val = data.split('\t')
+		#	hash_array.append(val[1])
+		#	name_array.append(val[2])
+		#print hash_array
+		#print name_array
+
+		s.send('send hashes')
+		file_array = []
+		hash_array = []
+		while True:
+			data = s.recv(1024)
+			#print data
+			data1 = data.split('\t')
+			if data == 'done':
+				break
+			s.send('recieved')
+			file_array.append(data1[1])
+			hash_array.append(data1[0])
+		i= 0
+		while i<len(file_array):
+			x = hash_array[i]
+			if x == '-1':
+				directorydownload(s,'TCP',file_array[i])
+			else:
+				args = 'download TCP ' + file_array[i] 
+				result = filedownload(s,args,file_array[i],'TCP')
+				#print result
+				hash_val = hash_value_func(file_array[i])
+				result1 = result.split()
+				if result1[0] == 'nodatasent':
+					print( file_array[i] + ':file already existing in the folder and uptodate')
+				else:
+					if result1[0]!=hash_val:
+						print(file_array[i] + ':download failed hash not same')
+					elif result1[0]==hash_val:
+						data = s.recv(1024)
+						#print('hash:' + result + '\t' + data)
+						#print 'Downloading completed successfully'
+						print(file_array[i] + ':file downloaded successfully')
+			i = i+1
+		print 'sync done'
+		strt_flag = 0
+		#s.send('over')					
+		continue
+
+
+	elif strt_flag == 0:
+		args = raw_input("prompt> ")
+		div = args.split()
+		#print div[0]
+		#print div[1]
+		#print div[2]
+		if len(div)==0 or div[0]=='close':
+			s.send(args)
+			print 'no command entered'
+			s.close()
+		elif div[0]=='download':
+			#print 'ab'
+			s.send('send hashes')
+			file_array = []
+			hash_array = []
+			while True:
+				data = s.recv(1024)
+				#print data
+				data1 = data.split('\t')
+				if data == 'done':
+					break
+				s.send('recieved')
+				file_array.append(data1[1])
+				hash_array.append(data1[0])
+			#print file_array
+			#print hash_array
+
+
+			filename = " ".join(div[2:])
+			#s.send('send')
+			i=0
+			while i<len(file_array):
+				if filename == file_array[i]:
+					hash_value = hash_array[i]
+					if hash_value == '-1':
+						#print 'ent dir'
+						protocol = div[1]
+						directorydownload(s,protocol,file_array[i])
+					else:
+						result = filedownload(s,args, filename, div[1]) #div[1] refers to UDP/TCP and div[2] refers to filename to be downloaded
+						#print('result=%s', (result))
+						# now we will check whether the recieved file hash value is same as the sent file hash value
+						fil = open(filename, 'rb') # rb indicate - open a file for reading
+						sent_hash = hashlib.md5(fil.read()).hexdigest()
+						result1 = result.split()
+						#print result1
+						if result1[0] == 'nodatasent':
+							command = 'chmod ' + result1[1] + ' ' + filename
+							#print command
+							os.system(command)
+							print 'file already existing in the folder and uptodate'
+						else:
+							if result != sent_hash:
+								print 'download failed hash not same'
+							else:
+								#s.send('send_status')
+								data = s.recv(1024)
+								#print data
+								spl = data.split('\t')
+								perm = spl[4]
+								#print('permissions:' + perm)
+								command = 'chmod ' +perm + ' ' + filename
+								#print command
+								os.system(command)
+								permissions = str(oct(os.stat(filename)[ST_MODE])[-3:])
+								#print permissions
+								#print data
+								print('hash:' + result + '\t' + data)
+								#print 'Downloading completed successfully'
+								print 'file downloaded successfully'
+					break
+				i = i+1
+
+		elif div[0]=='hash':
+			if div[1] == 'verify':
+				filename = " ".join(div[2:])
+				#print filename
+				s.send(args)
+				data = s.recv(1024)
+				#print data
+				if data != 'recieved':
+					print data
+				result = hash_verify(s,filename)
+				s.send("recieved")
+				#timestamp = s.recv(1024)
+				print('hash:' + result )
+				#print('last modified time:' +timestamp)
+			elif div[1] == 'checkall':
+				s.send(args)
+				data = s.recv(1024)
+				#print data
+				if data != 'recieved':
+					print data
+				while True:
+					data = s.recv(1024)
+					if data == 'done':
+						break
+					s.send('recieving')
+					print data
+		elif div[0] == 'index':
+			#print 'entered'
+			if div[1] == 'longlist':
+				#print 'entered 2'
+				s.send(args)
+				data = s.recv(1024)
+				if data != 'recieved':
+					print data
+				while True:
+					data = s.recv(1024)
+					if data == 'done':
+						break
+					s.send('recieving')
+					print data
+			elif div[1] == 'shortlist' and len(div) == 4:
+				#print 'ent'
+				s.send(args)
+				data = s.recv(1024)
+				if data != 'recieved':
+					print data
+				while True:
+					data = s.recv(1024)
+					if data == 'done':
+						break
+					s.send('recieving')
+					print data
+			elif div[1] == 'regex' and len(div) == 3:
+				s.send(args)
+				data = s.recv(1024)
+				if data != 'recieved':
+					print data
+				while True:
+					data = s.recv(1024)
+					if data == 'done':
+						break
+					s.send('recieving')
+					print data
+			else:
+				print 'input format wrong'
+
+
+s.close()
---
+++ shared_folder2/server2.py
@@ -0,0 +1,414 @@
+import socket
+import os
+import hashlib
+import time
+import platform
+import random
+from datetime import datetime
+import re
+import json
+from stat import *
+def create_port(socket):
+    np = random.randint(1000, 9999)
+    try:
+        socket.bind((host, np))
+    except:
+        return create_port(socket)
+    return np
+def shortlist(s,args):
+	s.send('recieved')
+	div = args.split()
+	strt_time = int(div[2])
+	end_time = int(div[3])
+	cmd = "ls"
+	result = os.popen(cmd).read()
+	files = result.split()
+	#print files
+	num = len(files)
+	i=0
+	while i<num:
+		filename = files[i]
+		Timestamp = time.ctime(os.path.getmtime(files[i]))
+		divide = Timestamp.split()
+		month = divide[1]
+		time1 = divide[3]
+		date = int(divide[2])
+		spl = time1.split(':')
+		hour = int(spl[0])
+		minut = int(spl[1])
+		sec = int(spl[2])
+		if month == 'Mar':
+			val = 3
+		elif month == 'Apr':
+			val = 4
+		tottime = val*30*24*60*60 + date*24*60*60 + hour*60*60 + minut*60 + sec #total time in seconds
+		#print tottime
+		if tottime >= strt_time and tottime <= end_time:
+			fil = '"' + filename + '"'
+			cmd = "stat --printf 'Type: %F\t' " + fil
+			statbuff = os.stat(filename)
+			size = str(statbuff.st_size)
+			Type = os.popen(cmd).read()
+			totstr = 'name:'+filename + '\t' +'size:' + size + '\t' + Type + '\t' + 'timestamp:' + Timestamp
+			#print totstr
+			s.send(totstr)
+			#print totstr
+			data = s.recv(1024)
+		i = i+1
+	s.send('done')
+
+def regex(s,args):
+	s.send('recieved')
+	div = args.split()
+	regx = div[2]
+	#print regx
+	cmd = 'ls'
+	result = os.popen(cmd).read()
+	files = result.split()
+	num = len(files)
+	i=0
+	while i<num:
+		if re.search(regx,files[i]) != None and re.search(regx,files[i]) != '':
+			filename = files[i]
+			Timestamp = time.ctime(os.path.getmtime(files[i]))
+			statbuff = os.stat(files[i])
+			size = str(statbuff.st_size)
+			fil = '"' + filename + '"'
+			cmd = "stat --printf 'Type: %F\t' " + fil
+			Type = os.popen(cmd).read()
+			totstr = 'name:'+filename + '\t' +'size:' + size + '\t' + Type + '\t' + 'timestamp:' + Timestamp
+			s.send(totstr)
+			data = s.recv(1024)
+		i = i+1
+	s.send('done')
+
+def longlist(s,args):
+	s.send("recieved")
+	cmd = "ls"
+	result = os.popen(cmd).read()
+	files = result.split()
+	#print files
+	num = len(files)
+	i=0
+	while i<num:
+		filename = files[i]
+		statbuff = os.stat(files[i])
+		size = str(statbuff.st_size)
+		Timestamp1 = str(statbuff.st_mtime)
+		Timestamp = time.ctime(os.path.getmtime(files[i]))
+		#print Timestamp
+		fil = '"' + filename + '"'
+		cmd = "stat --printf 'Type: %F\t' " + fil
+		Type = os.popen(cmd).read()
+		totstr = 'name:'+filename + '\t' +'size:' + size + '\t' + Type + '\t' + 'timestamp:' + Timestamp
+		s.send(totstr)
+		#print totstr
+		data = s.recv(1024)
+		i = i+1
+	s.send('done')
+
+def send_filehash(s , filename):
+	s.send("recieved")
+	fil = '"' + filename + '"'
+	cmd = "stat --printf '%F' " + fil
+	Type = os.popen(cmd).read()
+	if Type == 'directory':
+		file_hash = '-1'
+	elif Type == 'regular file':
+		file_hash = os.popen('cksum "' + filename + '"').read().split()[0]
+	Timestamp = time.ctime(os.path.getmtime(filename))
+	totstr = file_hash + '\t' + 'lastmodifiedtime:' + Timestamp
+	s.send(totstr)
+	data = s.recv(1024)
+	if data == 'recieved':
+		print 'done sending hash'
+	#command = "stat --printf 'Timestamp:%z\n' " + filename
+	#statbuff = os.stat(filename)
+	#stat = statbuff.st_mtime
+	#result = str(stat)
+	#result = os.popen(command).read()
+	#s.send(result)
+		#s.send('done')
+def send_allhash(s,checker):
+	#print 'abc'
+	#print checker
+	checker = str(checker)
+	s.send("recieved")
+	cmd = "ls"
+	result = os.popen(cmd).read()
+	files = result.split()
+
+	#print files
+	num = len(files)
+	i=0
+	while i<num:
+		fil = '"' + files[i] + '"'
+		cmd = "stat --printf '%F' " + fil
+		Type = os.popen(cmd).read()
+		if Type == 'directory':
+			hash_value = '-1'
+		elif Type == 'regular file':
+			hash_value = os.popen(checker + ' ' + '"' + files[i] + '"').read().split()[0]
+		#print hash_value
+		#command = "stat --printf 'Timestamp:%z\n' " + files[i]
+		statbuff = os.stat(files[i])
+		stat = statbuff.st_mtime
+		Timestamp = time.ctime(os.path.getmtime(files[i]))
+		res = 'lastmodifiedtime:' + Timestamp + '\t' + hash_value + '\t' + files[i]
+		#res = os.popen(command).read()
+		s.send(res)
+		#print res
+		data = s.recv(1024)
+		#print data
+		#if data == 'gotit':
+		#	print 'sent modified time'
+		#s.send(files[i])
+		#data1 = s.recv(1024)
+		#if data == 'gotfile':
+		#	print 'sent filename'
+		i = i+1
+	s.send('done')
+def sendfile(s , args):
+	div = args.split()
+	protocol = div[1]
+	filename = " ".join(div[2:])
+	if (os.popen('ls "' + filename + '"').read().split('\n')[0] == ""):
+		print 'file not opened'
+		print 'No Such File or Directory'
+		return
+	data = s.send("recieved")
+	#print data
+	if protocol == 'UDP':
+		ns = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		new_port = create_port(ns)
+		s.send(str(new_port))
+		data, addr = ns.recvfrom(1024)
+		print data
+		if data == "recieved":
+			file_hash = os.popen('md5sum "' + filename + '"').read().split()[0] 
+			ns.sendto(file_hash,addr)
+			#print 'passed'
+			confirm1 = ns.recvfrom(1024)
+			#print 'passes'
+			confirm = confirm1[0]
+			#print confirm
+			if confirm == 'hashsame':
+				perm1 = str(oct(os.stat(filename)[ST_MODE])[-3:])
+				#print perm1
+				perm = 'nodatasent ' + perm1
+				ns.sendto(perm,addr)
+			elif confirm == 'hashnotsame' or confirm == 'newfile':
+				try:
+					f = open(filename, "rb")
+					byte = f.read(1024)
+					#print byte
+					while byte:
+						#print byte
+						ns.sendto(byte, addr)
+						data, addr = ns.recvfrom(1024)
+						print data
+						if data != "recieved":
+							break
+						byte = f.read(1024)
+					ns.sendto("done", addr)
+					command = "stat --printf 'name: %n \tSize: %s bytes\t Timestamp:%z\t' " + filename
+					result1 = os.popen(command).read()
+					perm = str(oct(os.stat(filename)[ST_MODE])[-3:])
+					#print perm
+					result = result1 + '\t' + perm
+					#print result
+					s.send(result)
+				except:
+					print "Bad Connection Error"
+					return
+	if protocol == "TCP":
+		#calculate hash and send to client through socket
+		file_hash = os.popen('md5sum "' + filename + '"').read().split()[0] # command for calculating md5sum
+		s.send(file_hash)
+		confirm = s.recv(1024)
+		if confirm == 'hashsame':
+			perm1 = str(oct(os.stat(filename)[ST_MODE])[-3:])
+			#print perm1
+			perm = 'nodatasent ' + perm1
+			s.send(perm)
+		elif confirm == 'hashnotsame' or confirm == 'newfile':
+			try:
+				fil = open(filename, "rb")
+				byte_wise = fil.read(1024)
+				while byte_wise:
+					s.send(byte_wise)
+					if s.recv(1024) != "recieved":
+						break
+					byte_wise = fil.read(1024)
+				s.send("done")
+				command = "stat --printf 'name: %n \tSize: %s bytes\t Timestamp:%z\t' " + filename
+				result1 = os.popen(command).read()
+				perm = str(oct(os.stat(filename)[ST_MODE])[-3:])
+				#print perm
+				result = result1 + '\t' + perm
+				#print result
+				s.send(result)
+				print 'Done'
+			except:
+				print "Error in connection"
+				return
+	if protocol!='UDP' and protocol!='TCP':
+		print 'Wrong arguments'
+def sendhashes(s):
+	cmd = 'ls'
+	result = os.popen(cmd).read()
+	files = result.split()
+	i=0
+	while i<len(files):
+		fil = '"' + files[i] + '"'
+		cmd = "stat --printf '%F' " + fil
+		Type = os.popen(cmd).read()
+		#Type = Type1.split(':')
+		print Type
+		if Type == 'regular file':
+			file_hash = os.popen('md5sum "' + files[i] + '"').read().split()[0] # command for calculating md5sum
+			totstr = file_hash + '\t' + files[i]
+			s.send(totstr)
+		elif Type == 'directory':
+			file_hash = '-1'
+			s.send(file_hash + '\t' + files[i])
+		data = s.recv(1024)
+		i = i+1
+	s.send('done')
+
+def senddirectory(s,args):
+	div = args.split()
+	directory = " ".join(div[2:])
+	print directory
+	if (os.popen('ls "' + directory + '"').read().split('\n')[0] == ""):
+		print 'no such directory'
+		return
+	hash_value = -1
+	cmd = 'cd ' + directory
+	os.system(cmd)
+	command = 'ls'
+	files = os.popen(command).read()
+	i=0
+	while i<len(files):
+		args = 'download TCP' + files[i]
+		fil = '"' + files[i] + '"'
+		cmd = "stat --printf 'Type: %F\t' " + fil
+		Type = os.popen(cmd).read()
+		if Type == 'regular file':
+			sendfile(s,args,files[i])
+		elif Type == 'directory':
+			senddirectory(s,args)
+		i = i+1
+
+	#data = s.recv(1024)
+	#print data
+	#if data == 'send_status':
+	#	s.send(result)
+	
+	#	print "Done"
+
+s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+host = "0.0.0.0"
+port = input("Port: ") # Enter the port number
+#print port
+#port = 6001
+try:
+	s.bind((host, port))
+except:
+	print "Socket creation Error"
+	exit(0)
+s.listen(5)
+
+sharedfolder2 = raw_input("Enter full path of the folder to share with 1: ")
+
+if not os.path.exists(sharedfolder2):
+	print "No Such Folder"
+	exit(0)
+elif not os.access(sharedfolder2, os.R_OK):
+	print "No Privilleges"
+	exit(0)
+else:
+	os.chdir(sharedfolder2)
+print 'Directory changed'
+
+#try:
+#    log = open("server_log.log", "a+")
+#except:
+#    print "Cannot Open Log file"
+#    exit(0)
+print 'Server is up and listening....'
+
+while True:
+	try:
+		conn, addr = s.accept()
+	except:
+		s.close()
+	print 'Got connection from', addr
+	
+	while True:
+		#data = conn.recv(1024)
+		#print data
+		#conn.send('modeset')
+		#if data == '1':
+		try:
+			#print 'conn'
+			data = conn.recv(1024)
+			#print 'conn cont'
+		except:
+			conn.close()
+			print "connection closed to client"
+			break
+		div = data.split()
+		#print data
+			#print div[0]
+			#print div[1]
+			#print div[2]
+		if len(div) == 0 or div[0] == "over":
+			conn.close()
+			print "Connection closed to client"
+			break
+		elif div[0] == 'change' and div[1] == 'directory':
+			print 'entered'
+			directory = div[2]
+			#cmd = 'cd ' + directory
+			print directory
+			os.chdir(directory)
+			cmd = 'pwd'
+			cwd = os.popen(cmd).read()
+			print cwd
+			conn.send('changed')
+		elif div[0] == 'send' and div[1] == 'hashes':
+			print 'send all hashes'
+			sendhashes(conn)
+		elif div[0] == 'download':
+			#print 'download entered'
+			#filename = " ".join(div[2:])
+			#fil = '"' + filename + '"'
+			#cmd = "stat --printf 'Type: %F\t' " + fil
+			#Type = os.popen(cmd).read()
+			#if Type == 'regular file':
+			sendfile(conn, data)
+			#elif Type == 'directory':
+			#	senddirectory(conn,data)
+		elif div[0] == 'hash':
+			if div[1] == 'verify':
+				filename = " ".join(div[2:])
+				send_filehash(conn,filename)
+			elif div[1] == 'checkall':
+				send_allhash(conn,'cksum')
+		elif div[0] == 'index':
+			if div[1] == 'longlist':
+				longlist(conn,data)	
+			elif div[1] == 'shortlist' and len(div) == 4:
+				#print 'ent'
+				shortlist(conn,data)
+			elif div[1] == 'regex' and len(div) == 3:
+				regex(conn,data)
+		else:
+			conn.send("Invalid Command")
+			conn.send("done")
+	conn.close()
+
+
+
+
